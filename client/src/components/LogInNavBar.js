a0ae77a26b61f9009d248418320af2090e49b310
gamze <gamzekandara@gmail.com>
2025-02-01 20:32:37 -0500

    responsive text utilities added

--- client/src/components/LogInNavBar.js
+++ client/src/components/LogInNavBar.js
@@ -6,19 +6,19 @@ function LogInNavBar() {
   return (
     <div className="fixed top-0 w-full flex justify-between items-center sm:text-sm p-1 bg-gray-800 text-white shadow-md hover:shadow-red-900 mb-10">
       <div className="flex items-center">
-        <p className="font-bold ml-4 responsive-heading">SaleSight</p>
+        <p className="font-bold ml-4 responsive-text">SaleSight</p>
         <GrAnalytics className="ml-3 mt-4 transform -translate-y-1/2" />
       </div>
       <div className="flex space-x-6 mr-4">
         <NavLink
           to="/about"
-          className="px-2 py-2 bg-gray-700 rounded-lg hover:bg-gray-600 transition-colors"
+          className="px-2 py-2 bg-gray-700 responsive-text rounded-lg hover:bg-gray-600 transition-colors"
         >
           About
         </NavLink>
         <NavLink
           to="/login"
-          className="px-2 py-2 bg-gray-700 rounded-lg hover:bg-gray-600 transition-colors"
+          className="px-2 py-2 bg-gray-700 responsive-text rounded-lg hover:bg-gray-600 transition-colors"
         >
           Login
         </NavLink>
