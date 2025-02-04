62ce2c03d6be3114baa53c4b73927ce780001075
gamze <gamzekandara@gmail.com>
2025-02-04 15:03:04 -0500

    responsive text added

--- client/src/components/ProductToolBar.js
+++ client/src/components/ProductToolBar.js
@@ -7,10 +7,10 @@ function ProductToolBar({ handleAddProfit }) {
   };
 
   return (
-    <div className="bg-white p-4 flex flex-col items-start w-full sm:text-sm mt-12">
-      <p className="text-xs font-semibold mb-2">Manage your store</p>
+    <div className="bg-white p-4 flex flex-col items-start w-full responsive-text mt-12">
+      <p className="responsive-text font-semibold mb-2">Manage your store</p>
       <NavLink
-        className="bg-white text-xs text-gray-700 hover:underline"
+        className="bg-white responsive-text text-gray-700 hover:underline"
         onClick={handleClick}
       >
         Add Profit Data
--- client/src/pages/AnalyticsPage.js
+++ client/src/pages/AnalyticsPage.js
@@ -13,13 +13,13 @@ function AnalyticsPage() {
   return (
     <>
       <MainNavBar />
-        <div className="h-content-height flex-col flex items-center justify-center mb-1 transition-shadow duration-300 ease-in-out hover:shadow-red-900">
+        <div className="h-content-height responsive-text flex-col flex items-center justify-center mb-1 transition-shadow duration-300 ease-in-out hover:shadow-red-900">
           <h1 className="sm:text-sm font-bold text-gray-800 mt-0 p-2">Profit Hub</h1>
           {/* welcome message for user if authenticated and directed to Profit Hub */}
           <p className="text-blue-600 text-center text-xs font-bold w-full max-w-2xl mt-2">
             {currentUser ? `Welcome ${currentUser.username}!` : null}
           </p>
-          <p className="text-gray-700 text-center text-xs w-full max-w-2xl mt-2 p-2">
+          <p className="text-gray-700 text-center responsive-text w-full max-w-2xl mt-2 p-2">
             Your one-stop solution for managing your sales and earnings. Here
             you can view your total profit, revenue, and total cost data
             analytics.
