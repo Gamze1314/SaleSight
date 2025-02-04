3ec9143337686fa464466db975523c692d7f9988
gamze <gamzekandara@gmail.com>
2025-02-03 21:47:04 -0500

    responsive text added in MainNavBar

--- client/src/components/MainNavBar.js
+++ client/src/components/MainNavBar.js
@@ -15,25 +15,25 @@ function MainNavBar() {
   return (
     <div className="fixed top-0 w-full flex justify-between items-center sm:text-sm p-1 bg-gray-800 text-white shadow-md hover:shadow-red-900">
       <div className="flex items-center ml-2 text-lg sm:text-sm">
-        <p className="sm:text-md font-bold ml-2">SaleSight</p>
+        <p className="sm:text-md responsive-text font-bold ml-2">SaleSight</p>
         <GrAnalytics className="ml-3 mt-4 transform -translate-y-1/2" />
       </div>
       <div className="flex space-x-6 mr-1 text-sm">
         <NavLink
           to="/profit_center"
-          className="px-2 py-2 bg-gray-700 rounded-lg hover:bg-gray-600 transition-colors"
+          className="px-2 py-2 responsive-text bg-gray-700 rounded-lg hover:bg-gray-600 transition-colors"
         >
           Profit Center
         </NavLink>
         <NavLink
           to="/my_store"
-          className="px-2 py-2 bg-gray-700 rounded-lg hover:bg-gray-600 transition-colors"
+          className="px-2 py-2 responsive-text bg-gray-700 rounded-lg hover:bg-gray-600 transition-colors"
         >
           My Store
         </NavLink>
         <NavLink
           onClick={handleLogOut}
-          className="px-2 py-2 bg-gray-700 rounded-lg hover:bg-gray-600 transition-colors"
+          className="px-2 py-2 responsive-text bg-gray-700 rounded-lg hover:bg-gray-600 transition-colors"
         >
           Log Out
         </NavLink>
